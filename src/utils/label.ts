1a258ba3db4e37c8abebe98fb53d223e1fdf8f90
Dokyun Lim <im.koremp@gmail.com>
2022-11-04 17:05:12 +0900

    feat: add functions to LabelCanvas

--- src/utils/label.ts
+++ src/utils/label.ts
@@ -1,40 +1,97 @@
 export interface LabelRect {
   x: number; // small x
   y: number; // small y
-  width: number;
-  height: number;
+  width: number; // x + width
+  height: number; // y + height
 }
 
-const LabelEvent = {
-  DefaultEvent: "DefaultEvent",
+export const LabelMode = {
+  ViewLabel: "ViewLabel",
   CreateLabel: "CreateLabel",
   SelectLabel: "SelectLabel",
-  MoveLabel: "MoveLabel",
-  AdjustLabelSize: "AdjustLabelSize",
-  RotateLabel: "RotateLabel",
-  DeleteLabel: "DeleteLabel",
 } as const;
 
-export type LabelEvent = typeof LabelEvent[keyof typeof LabelEvent];
+export type LabelMode = typeof LabelMode[keyof typeof LabelMode];
 
-export default class LabelCanvas {
-  width: number;
-  height: number;
+export const SelectedLabelEvent = {
+  MoveLabel: "MoveSelectedLabel",
+  AdjustLabelSize: "AdjustSelectedLabelSize",
+  RotateLabel: "RotateSelectedLabel",
+  DeleteLabel: "DeleteSelectedLabel",
+} as const;
+
+export type SelectedLabelEvent = typeof SelectedLabelEvent[keyof typeof SelectedLabelEvent];
+
+const LABEL_BOX_COLOR = '#5668D9';
+const BOX_OPACITY = 0.2;
+const BORDER_OPACITY = 1;
+const BORDER_WIDTH = 3; // 3px
+const ANCHOR_BOX_WIDTH = 16;
+const ANCHOR_BOX_HALF_WIDTH = 8;
+
+export class LabelCanvas {
   labels: LabelRect[];
-  mode: LabelEvent;
-  canvas: HTMLCanvasElement;
+  selectedLabels: LabelRect[];
+  labelMode: LabelMode;
+  selectedLabelEvent: SelectedLabelEvent | undefined;
   context: CanvasRenderingContext2D;
 
-  constructor(width: number, height: number) {
-    this.width = width;
-    this.height = height;
+  constructor(context: CanvasRenderingContext2D | null) {
+    if (!context) {
+      throw new Error('LabelCanvas constructor context is null!');
+    }
+
     this.labels = [];
-    this.mode = LabelEvent.DefaultEvent;
-    this.canvas = document.getElementById('label-canvas') as HTMLCanvasElement;
-    this.context = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    this.selectedLabels = [];
+    this.labelMode = LabelMode.ViewLabel;
+    this.selectedLabelEvent = undefined;
+    this.context = context;
+    this.context.fillStyle = LABEL_BOX_COLOR;
+    this.context.lineWidth = 3;
+  }
+
+  changeLabelMode(mode: LabelMode) {
+    this.labelMode = mode;
+
+    if (this.labelMode === LabelMode.ViewLabel) {
+      this.selectedLabels = [];
+      this.labels.forEach(({ x, y, width, height }) => {
+        // inside of label rect
+        this.context.globalAlpha = BOX_OPACITY;
+        this.context.fillRect(x + BORDER_WIDTH, y + BORDER_WIDTH, width - BORDER_WIDTH, height - BORDER_WIDTH);
+
+        // label rect border
+        this.context.globalAlpha = BORDER_OPACITY;
+        this.context.strokeRect(x, y, width, height);
+      })
+    }
+
+    if (this.labelMode === LabelMode.SelectLabel) {
+
+    }
+  }
+
+  addAnchorToSelectedLabels() {
+    this.selectedLabels.forEach(({ x, y, width, height }) => {
+      // label rect border anchors - 16 x 16, 8 anchors
+      this.context.strokeRect(x - ANCHOR_BOX_HALF_WIDTH, y - ANCHOR_BOX_HALF_WIDTH, ANCHOR_BOX_WIDTH, ANCHOR_BOX_WIDTH);
+      this.context.strokeRect(x - ANCHOR_BOX_HALF_WIDTH, y - ANCHOR_BOX_HALF_WIDTH + height / 2, ANCHOR_BOX_WIDTH, ANCHOR_BOX_WIDTH);
+      this.context.strokeRect(x - ANCHOR_BOX_HALF_WIDTH, y - ANCHOR_BOX_HALF_WIDTH + height, ANCHOR_BOX_WIDTH, ANCHOR_BOX_WIDTH);
+
+      this.context.strokeRect(x - ANCHOR_BOX_HALF_WIDTH + width / 2,
+        y - ANCHOR_BOX_HALF_WIDTH, ANCHOR_BOX_WIDTH, ANCHOR_BOX_WIDTH);
+      this.context.strokeRect(x - ANCHOR_BOX_HALF_WIDTH + width,
+        y - ANCHOR_BOX_HALF_WIDTH, ANCHOR_BOX_WIDTH, ANCHOR_BOX_WIDTH);
+
+      this.context.strokeRect(x - ANCHOR_BOX_HALF_WIDTH + width, y - ANCHOR_BOX_HALF_WIDTH + height, ANCHOR_BOX_WIDTH, ANCHOR_BOX_WIDTH);
+    })
   }
 
   createLabel(x: number, y: number, endX: number, endY: number) {
+    if (this.labelMode !== LabelMode.CreateLabel) {
+      return;
+    }
+
     const label: LabelRect = {
       x: x > endX ? endX : x,
       y: y > endY ? endY : y,
@@ -43,5 +100,51 @@ export default class LabelCanvas {
     }
 
     this.labels.push(label);
+    this.selectedLabels.push(label);
+    this.addAnchorToSelectedLabels();
+  }
+
+  selectLabel(x: number, y: number) {
+    if (this.labelMode !== LabelMode.SelectLabel) {
+      return;
+    }
+
+    // for (const label of this.labels.slice().reverse()) {
+    for (const label of this.labels) {
+      if (x >= label.x && x <= label.x + label.width
+        && y >= label.y && y <= label.y + label.height) {
+        this.selectedLabels.push(label);
+        this.addAnchorToSelectedLabels();
+        break;
+      }
+    }
+  }
+
+  moveLabel(startX: number, startY: number, endX: number, endY: number) {
+    if (this.labelMode !== LabelMode.SelectLabel) {
+      return;
+    }
+  }
+
+  onMouseDownCanvas(event: MouseEvent) {
+    if (this.labelMode === LabelMode.ViewLabel) {
+      return;
+    }
+
+    if (this.labelMode === LabelMode.CreateLabel) {
+
+    }
+
+    if (this.labelMode === LabelMode.SelectLabel) {
+
+    }
+  }
+
+  onMouseEnter() {
+
+  }
+
+  onMouseLeave() {
+
   }
 }
